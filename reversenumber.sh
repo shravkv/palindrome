ac50627e3eea078df4a375ab1af8d2591706fa23
sravan <sravan.k.vodnala@gmail.com>
2022-05-15 11:48:23 +0530

    first commit

---
+++ reversenumber.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo Enter Number to Reverse
+read number
+num=$number;
+
+reverse=0
+while [ $number -gt 0 ]
+do
+   rem='expr $number % 10'
+   reverse='expr $reverse \* 10 + $rem'
+   number='expr $number / 10'
+done
+
+echo Reverse=$reverse;
+
+if [ $num -eq $reverse ]
+then
+    echo "Palindrome!!!"
+else
+    echo "Not Palindrome"
+fi
+
